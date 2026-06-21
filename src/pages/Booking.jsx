63dbe3929161d6df4938f26f4e990aa9501add93
Olena Kohun <helenakohun@gmail.com>
2026-06-21 15:48:50 +0200

    fix: typo

--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -16,11 +16,12 @@ export default function Booking() {
 
     const year = today.getFullYear();
     const month = String(today.getMonth() + 1).padStart(2, "0");
-    const day = String(today.getDay()).padStart(2, "0");
+    const day = String(today.getDate()).padStart(2, "0");
 
     return `${year}-${month}-${day}`;
   }
   const today = getTodatDateString();
+  console.log(today);
 
   //Restaurant info
   const info = [
