e227f8405786ef16baa0f557fe5c9529d07d6101
Olena Kohun <helenakohun@gmail.com>
2026-07-03 23:47:59 +0200

    Add HelmetProvider component

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { HelmetProvider } from "react-helmet-async";
 import "./index.css";
 import App from "./App.jsx";
 import "./i18n/index.js";
 import "leaflet/dist/leaflet.css";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <HelmetProvider>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </HelmetProvider>,
 );
