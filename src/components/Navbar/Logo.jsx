c288f0f24a810deda9a3417a95e4f61a4fd520ce
Olena Kohun <helenakohun@gmail.com>
2026-07-08 16:08:14 +0200

    fix: small issues before deploy

--- src/components/Navbar/Logo.jsx
+++ src/components/Navbar/Logo.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Logo() {
   return (
     <Link to="/">
-      <span className="font-heading text-2xl font-light tracking-[0.2em] text-white lg:text-3xl">
+      <span className="font-heading flex flex-nowrap text-xl font-light tracking-[0.2em] text-white lg:text-3xl">
         S U L I K O
       </span>
     </Link>
