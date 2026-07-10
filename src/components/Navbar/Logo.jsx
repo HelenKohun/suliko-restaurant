68603f43270cb34da27efbe4234b87f7784d1049
Olena Kohun <helenakohun@gmail.com>
2026-07-10 15:40:39 +0200

    fix: prevent logo text wrapping

--- src/components/Navbar/Logo.jsx
+++ src/components/Navbar/Logo.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Logo() {
   return (
     <Link to="/">
-      <span className="font-heading flex flex-nowrap text-xl font-light tracking-[0.2em] text-white lg:text-3xl">
+      <span className="font-heading flex text-xl font-light tracking-[0.2em] whitespace-nowrap text-white md:text-2xl lg:text-3xl">
         S U L I K O
       </span>
     </Link>
